95ed02678d981332fe8fce7daa68e20f829bd396
zhangwx <zhangwxgo@gmail.com>
2017-05-17 23:05:03 +0800

    【+】跑通 get

--- app/src/main/java/com/zhangwx/z_okhttp/MainActivity.java
+++ app/src/main/java/com/zhangwx/z_okhttp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.zhangwx.z_okhttp;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -8,16 +10,30 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private OKHttpHelper mOkHttpHelper;
+    private Handler mHandler = new Handler(Looper.getMainLooper());
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mOkHttpHelper = new OKHttpHelper();
         findViewById(R.id.get).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView textView = (TextView) findViewById(R.id.content);
-                textView.setText(mOkHttpHelper.get());
+                mOkHttpHelper.get(new OKHttpHelper.ResponseListener() {
+                    @Override
+                    public void onResponse(final String result) {
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                TextView textView = (TextView) findViewById(R.id.content);
+                                textView.setText(result);
+                            }
+                        });
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/zhangwx/z_okhttp/OKHttpHelper.java
+++ app/src/main/java/com/zhangwx/z_okhttp/OKHttpHelper.java
@@ -2,40 +2,47 @@ package com.zhangwx.z_okhttp;
 
 import java.io.IOException;
 
+import android.os.Handler;
+import android.os.Looper;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 /**
  * Created by zhangwx on 2017/5/16.
- *
+ * helper
  */
 
 public class OKHttpHelper {
 
-    public interface OnResponse{
-
+    public OKHttpHelper() {
     }
 
-
-    public String get() {
-
+    public void get(final ResponseListener listener) {
         BackgroundThread.post(new Runnable() {
             @Override
             public void run() {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder().url("http://www.baidu.com").build();
+                try {
+                    Response response = client.newCall(request).execute();
+                    if (response.isSuccessful()) {
+                        final ResponseBody body = response.body();
 
+
+
+                        listener.onResponse(body.string());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url("http://www.baidu.com").build();
-        try {
-            Response response = client.newCall(request).execute();
-            if (response.isSuccessful()) {
-                return request.body().toString();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "Error";
+    }
+
+    public interface ResponseListener {
+        void onResponse(String result);
     }
 }
