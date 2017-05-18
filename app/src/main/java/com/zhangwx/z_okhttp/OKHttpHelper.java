6f7553357dac527b10dacc6314655b147eb9c604
zhangwx <zhangwxgo@gmail.com>
2017-05-18 09:27:17 +0800

    【+】post

--- app/src/main/java/com/zhangwx/z_okhttp/OKHttpHelper.java
+++ app/src/main/java/com/zhangwx/z_okhttp/OKHttpHelper.java
@@ -4,11 +4,17 @@ import java.io.IOException;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.support.annotation.Nullable;
 
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
+import okio.BufferedSink;
 
 /**
  * Created by zhangwx on 2017/5/16.
@@ -29,15 +35,40 @@ public class OKHttpHelper {
                 try {
                     Response response = client.newCall(request).execute();
                     if (response.isSuccessful()) {
-                        final ResponseBody body = response.body();
-
-
-
-                        listener.onResponse(body.string());
+//                        final ResponseBody body = response.body();
+                        listener.onResponse(response.message());
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+            }
+        });
+    }
+
+    public void post(final ResponseListener listener) {
+        BackgroundThread.post(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                FormBody.Builder builder = new FormBody.Builder();
+//                builder.add()
+                RequestBody requestBody = builder.build();
+
+                Request request = new Request.Builder()
+                        .url("http://www.baidu.com")
+                        .post(requestBody)
+                        .build();
+
+                Response response = null;
+                try {
+                    response = client.newCall(request).execute();
+                    if (response.isSuccessful()) {
+                        listener.onResponse(response.message());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
     }
